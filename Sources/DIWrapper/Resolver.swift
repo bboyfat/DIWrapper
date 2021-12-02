578f85bcfd2183ab6435e1a587f04b481254c4b8
Andrii_Petrovskyi <>
2021-12-02 23:36:42 +0200

    - updated modificators

--- Sources/DIWrapper/Resolver.swift
+++ Sources/DIWrapper/Resolver.swift
@@ -9,15 +9,15 @@ import Foundation
 
 public class Resolver {
 
-    static let shared = Resolver()
-    var factoryDict: [String: () -> Any] = [:]
+    public static let shared = Resolver()
+    private var factoryDict: [String: () -> Any] = [:]
 
-    func add<T>(type: T.Type, _ factory: @escaping () -> T) {
+    public func add<T>(type: T.Type, _ factory: @escaping () -> T) {
         factoryDict[String(describing: type.self)] = factory
     }
 
     @discardableResult
-    func resolve<T>(_ type: T.Type) -> T {
+    public func resolve<T>(_ type: T.Type) -> T {
         if let component: T = factoryDict[String(describing: T.self)]?() as? T {
         return component
         } else {
